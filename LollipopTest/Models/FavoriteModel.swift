7ab81ad46b9b38a4edf632a02dbc178d2c4e373f
Bobson <kikyou77111@gmail.com>
2022-07-01 00:04:20 +0800

    Add Realm的data model包住WebAPI model

--- LollipopTest/Models/FavoriteModel.swift
+++ LollipopTest/Models/FavoriteModel.swift
@@ -6,17 +6,43 @@
 //
 
 import Foundation
+import RealmSwift
 
-struct FavoriteModel: Codable, Comparable {
+class FavoriteModel: Object, Comparable {
     
     /// 加入時間，排序用
-    let updateTime: Double
+    @Persisted var updateTime: Double = 0.0
+
+    /// e.g. "Gaming"
+    @Persisted var subreddit: String = ""
     
-    let model: HotModel.Data.Children.Data
+    /// e.g. "Geese enjoying a harmonica performance at Daan Forest Park in Taipei"
+    @Persisted var title: String = ""
     
-    init(model: HotModel.Data.Children.Data) {
+    /// e.g. "https://b.thumbs.redditmedia.com/viz_BReZL1pcaz4FdKQGePoiOEIB5LKVd_6XMjPSXxI.jpg"
+    @Persisted var thumbnail: String = ""
+    
+    /// e.g. "vl2uf9"
+    @Persisted var id: String = ""
+    
+    var model: HotModel.Data.Children.Data {
+        return HotModel.Data.Children.Data(subreddit: subreddit, title: title, thumbnail: thumbnail, id: id)
+    }
+
+    /// 沒override會crash
+    override init() {
+        super.init()
+    }
+    
+    convenience init(model: HotModel.Data.Children.Data) {
+        self.init()
+        
         self.updateTime = Date().timeIntervalSince1970
-        self.model = model
+
+        self.subreddit = model.subreddit
+        self.title = model.title
+        self.thumbnail = model.thumbnail
+        self.id = model.id
     }
 
     static func < (lhs: FavoriteModel, rhs: FavoriteModel) -> Bool {
